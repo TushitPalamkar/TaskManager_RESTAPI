f57a4776f8a09eee1202d65324f5642925ff8076
TushitPalamkar <143091687+TushitPalamkar@users.noreply.github.com>
2024-06-17 18:00:14 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const connectionString='mongodb+srv://Tushit:tushit%40123@nodeexpressprojects.wanoc4c.mongodb.net/?retryWrites=true&w=majority&appName=TaskManagerNode'
+
 
 const connectDB=(url)=>{
     return mongoose.connect(url).then(()=>console.log('Successfull')).catch((err)=>console.log(err))
